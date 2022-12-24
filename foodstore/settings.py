e5f3b87d6d128a83303dad058083a14701031c92
KMaina-N <kelvinmaina350@gmail.com>
2022-12-24 16:59:46 +0100

    hosts

--- foodstore/settings.py
+++ foodstore/settings.py
@@ -26,9 +26,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-bu%1d$@_w9adpx27j0l4z%jufrms8^1#fdde8@*mz*$nxuqbkz'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['africanfoodstorezagreb.herokuapp.com']
 
 
 # Application definition
